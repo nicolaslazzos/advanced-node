0c17a4ab29a6f1173d4b1d656a050221babee84c
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-06-10 21:24:52 -0300

    updating packages and fixing things

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,76 +1,75 @@
-const Page = require('./helpers/page');
+const Page = require("./helpers/page");
 
 let page;
 
 beforeEach(async () => {
   page = await Page.build();
-  await page.goto('http://localhost:3000');
+  await page.goto("http://localhost:3000");
 });
 
 afterEach(async () => {
   await page.close();
 });
 
-describe('when logged in', async () => {
+describe("when logged in", () => {
   beforeEach(async () => {
     await page.login();
-    await page.click('a.btn-floating');
+    await page.click("a.btn-floating");
   });
 
-  test('can see blog creation form', async () => {
-    const label = await page.getContentOf('form label');
-    expect(label).toEqual('Blog Title');
+  test("can see blog creation form", async () => {
+    const label = await page.getContentOf("form label");
+    expect(label).toEqual("Blog Title");
   });
 
-  describe('and using valid inputs', async () => {
+  describe("and using valid inputs", () => {
     beforeEach(async () => {
-      await page.type('.title input', 'My Title');
-      await page.type('.content input', 'My Content');
-      await page.click('form button');
+      await page.type(".title input", "My Title");
+      await page.type(".content input", "My Content");
+      await page.click("form button");
     });
 
-    test('submitting takes user to review screen', async () => {
-      const text = await page.getContentOf('h5');
-      expect(text).toEqual('Please confirm your entries');
+    test("submitting takes user to review screen", async () => {
+      const text = await page.getContentOf("h5");
+      expect(text).toEqual("Please confirm your entries");
     });
 
-    test('submitting then saving adds blog to index page', async () => {
-    });
-  })
+    test("submitting then saving adds blog to index page", async () => {});
+  });
 
-  describe('and using invalid inputs', async () => {
+  describe("and using invalid inputs", () => {
     beforeEach(async () => {
-      await page.click('form button');
+      await page.click("form button");
     });
 
-    test('the form shows an error message', async () => {
-      const titleError = await page.getContentOf('.title .red-text');
-      const contentError = await page.getContentOf('.content .red-text');
+    test("the form shows an error message", async () => {
+      const titleError = await page.getContentOf(".title .red-text");
+      const contentError = await page.getContentOf(".content .red-text");
 
-      expect(titleError).toEqual('You must provide a value');
-      expect(contentError).toEqual('You must provide a value');
+      expect(titleError).toEqual("You must provide a value");
+      expect(contentError).toEqual("You must provide a value");
     });
   });
 });
 
-describe('when not logged in', async () => {
+describe("when not logged in", () => {
   const actions = [
     {
-      method: 'get',
-      path: '/api/blogs'
+      method: "get",
+      path: "/api/blogs",
     },
     {
-      method: 'post',
-      path: '/api/blogs',
+      method: "post",
+      path: "/api/blogs",
       data: {
-        title: 'My Title',
-        content: 'My Content'
-      }
-    }
+        title: "My Title",
+        content: "My Content",
+      },
+    },
   ];
 
-  test('blog related actions are prohibited', async () => {
+  test("blog related actions are prohibited", async () => {
     const results = await page.execRequests(actions);
-    results.forEach(result => expect(result).toEqual({ error: 'You must log in!' }));
+    results.forEach((result) => expect(result).toEqual({ error: "You must log in!" }));
   });
 });
--- tests/setup.js
+++ tests/setup.js
@@ -2,10 +2,10 @@
 // the tests, in order to, for example, be able to interact with the database and use mongoose models
 jest.setTimeout(30000);
 
-require('../models/User');
+require("../models/User");
 
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useMongoClient: true });
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
