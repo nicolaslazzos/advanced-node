c1ee133611882241d74292702c618ca3d5e46713
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-06-12 22:46:08 -0300

    disconnecting database after all tests

--- tests/setup.js
+++ tests/setup.js
@@ -9,3 +9,7 @@ const keys = require("../config/keys");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+afterAll(() => {
+  mongoose.disconnect();
+});
